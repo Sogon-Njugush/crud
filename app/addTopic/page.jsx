2992abe549da07a207ad5ae30c217f120a5f5f9e
Sogon-Njugush <sogon5045@gmail.com>
2024-05-03 10:52:44 +0300

    final intergration

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -1,13 +1,49 @@
-import React from 'react'
+"use client"
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
 
-const pages = () => {
+export default function pages () {
+
+  const  [title, setTitle] = useState('')
+  const [description, SetDescription] = useState('')
+
+  const router = useRouter()
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+
+    if(!title || !description){
+      alert("Title and decription are required")
+      return;
+    }
+
+    try {
+      const res = await fetch('http://localhost:3000/api/topics',{
+        method: "POST",
+        headers:{
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({title, description}),
+      });
+
+      if(res.ok){
+        router.push('/');
+      }else{
+        throw new Error("Failed to create a topic")
+      }
+      
+    } catch (error) {
+      console.log("error")
+    }
+
+  }
+
   return (
-        <form className='flex flex-col gap-3'>
-          <input className='border border-slate-500 px-8 py-2' type='text' placeholder='Topic Title..'/>
-          <input className='border border-slate-500 px-8 py-2' type='text' placeholder='Topic Description..'/>
-          <button className='bg-green-600 font-bold text-white py-3 px-6 w-fit'>Add Topic</button>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-3'>
+          <input onChange={(e)=>setTitle(e.target.value)} value={title} className='border border-slate-500 px-8 py-2' type='text' placeholder='Topic Title..'/>
+          <input onChange={(e)=>SetDescription(e.target.value)} value={description} className='border border-slate-500 px-8 py-2' type='text' placeholder='Topic Description..'/>
+          <button type='submit' className='bg-green-600 font-bold text-white py-3 px-6 w-fit'>Add Topic</button>
       </form>    
   )
 }
 
-export default pages
--- libs/mongodb.js
+++ libs/mongodb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectMongoDB = async() => {
     try {
        await mongoose.connect(process.env.MONGODB_URL);
-        console.log("conncetion successful");
+        // console.log("conncetion successful");
     } catch (error) {
         console.log("failed to connect")
     }    
